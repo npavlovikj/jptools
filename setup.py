13d0bacb0286a36a40009207967de747b3e36e47
Natasha Pavlovikj <natasha.pavlovikj@yahoo.com>
2017-03-31 12:55:10 -0500

    Update setup.py
    Remove dependency list

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup
 def readme():
     with open('README') as f:
         return f.read()
-
-
-
 setup(name='jptools',
       version='0.01',
       description='For Oxford',
@@ -21,11 +18,6 @@ setup(name='jptools',
         "pbcore >= 0.6.3",
         "pbtools.pbdagcon >= 0.2.3",
         ],
-      dependency_links = [
-        "git+https://github.com/jgurtowski/jbio#egg=jbio-0.1",
-        "git+https://github.com/PacificBiosciences/pbcore#egg=pbcore-0.6.3",
-        "git+https://github.com/PacificBiosciences/pbdagcon#egg=pbtools.pbdagcon-0.2.3"
-        ],
       entry_points = {
         'console_scripts': [
             'coverageFromBlast6 = jptools.coverage:coverage_from_blast6',
@@ -35,7 +27,3 @@ setup(name='jptools',
             ]
         },
       )
-      
-      
-      
-      
